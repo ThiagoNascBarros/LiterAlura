41adafe4bb7c5ee8fbcb28600509f855f95c5cc8
ThiagoNascBarros <thiago171nasciment@gmail.com>
2026-01-19 01:46:01 -0300

    feat: Using commandline runner

--- src/main/java/br/com/literalura/LiteraluraApplication.java
+++ src/main/java/br/com/literalura/LiteraluraApplication.java
@@ -1,13 +1,20 @@
 package br.com.literalura;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Log4j2
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(LiteraluraApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Hello World");
+    }
 }
