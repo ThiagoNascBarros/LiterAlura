0faebcc1cbaa4533c74634f31bf3dc50609cef3c
ThiagoNascBarros <thiago171nasciment@gmail.com>
2026-01-22 11:38:05 -0300

    fix: Created query using JPQL for best search
    Signed-off-by: ThiagoNascBarros <thiago171nasciment@gmail.com>

--- src/main/java/br/com/literalura/application/ports/inbound/repository/IAuthorRepository.java
+++ src/main/java/br/com/literalura/application/ports/inbound/repository/IAuthorRepository.java
@@ -2,11 +2,16 @@ package br.com.literalura.application.ports.inbound.repository;
 
 import br.com.literalura.domain.entities.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface IAuthorRepository extends JpaRepository<Author, UUID> {
     Author findByName(String name);
+
+    @Query("select a from author a where a.birthYear <= :birthYear and (a.deathYear >= :deathYear or a.deathYear is null)")
+    List<Author> findByBirthYearAndDeathYearAfter(Integer birthYear, Integer deathYear);
 }
